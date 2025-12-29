c2b2bd75500aab938305d434d88074f60e929d9b
DESKTOP-FJ9HM40\Bocom <kuriloivan2003@gmail.com>
2025-12-29 22:24:17 +0200

    fixed

--- HW5/HW5_3.py
+++ HW5/HW5_3.py
@@ -3,9 +3,7 @@ import string
 user_str = input("Enter string: ").strip()
 
 cleand_str = ''.join(ch for ch in user_str if ch not in string.punctuation)
-
 hash_tag_str = '#' + ''.join(cleand_str.title().split())
-
 hash_tag_str = hash_tag_str[:140]
 
 print(hash_tag_str)
