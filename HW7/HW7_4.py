5bac62fae1805881d34fe63c8ebbe333b8440719
DESKTOP-FJ9HM40\Bocom <kuriloivan2003@gmail.com>
2026-01-06 19:44:55 +0200

    fixed HW7_4

--- HW7/HW7_4.py
+++ HW7/HW7_4.py
@@ -1,5 +1,5 @@
 def common_elements():
-    return {num for num in range(100) if num % 3 == 0 and num % 5 == 0}
+    return set(num for num in range(100) if num % 3 == 0) & set(num for num in range(100) if num % 5 == 0)
 
 
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
