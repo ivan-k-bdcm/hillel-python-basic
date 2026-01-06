abee32674ea4c9b97bbc61b5a08e1d46068fa55d
DESKTOP-FJ9HM40\Bocom <kuriloivan2003@gmail.com>
2026-01-06 19:42:20 +0200

    fixed HW7_4

--- HW7/HW7_4.py
+++ HW7/HW7_4.py
@@ -1,8 +1,5 @@
 def common_elements():
-    int_division_by_3 = (num for num in range(100) if num % 3 == 0)
-    int_division_by_5 = (num for num in range(100) if num % 5 == 0)
-
-    return set(int_division_by_3) & set(int_division_by_5)
+    return {num for num in range(100) if num % 3 == 0 and num % 5 == 0}
 
 
 assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
